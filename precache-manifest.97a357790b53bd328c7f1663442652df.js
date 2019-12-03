45a063ba879782fbb9c76fe2f6431e5349a06c92
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-03 13:36:49 +0200

    react decompose

---
+++ precache-manifest.97a357790b53bd328c7f1663442652df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54764b3f91fd1a3d4ca7288c9fc2d935",
+    "url": "./index.html"
+  },
+  {
+    "revision": "335edef8f18039e139b4",
+    "url": "./static/css/main.238437e6.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "./static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "335edef8f18039e139b4",
+    "url": "./static/js/main.b7afa695.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
