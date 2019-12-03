476846eb4f7abf3e46efa89e2ff82518391dd8e1
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-03 12:52:45 +0200

    decomposion success

---
+++ src/components/section/Section.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SectionStyles.css';
+
+function Section() {
+  return (
+    <>
+      <section className="welcome">
+        <span className="welcome__text">Sticky Header!</span>
+      </section>
+    </>
+  )
+}
+
+export default Section;
